f882c5bb6ceacd710da8caf085a81bd3cd20550f
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-26 04:12:56 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RandomlyGeneratable-module
+
+A description of this package.
---
+++ Sources/RandomlyGeneratable-module/RandomlyGeneratable_module.swift
@@ -0,0 +1,6 @@
+public struct RandomlyGeneratable_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
